cb0b37f336b4c066d480d02f9ff67537066eed78
Chandler Paul <thatslyude@gmail.com>
2013-06-17 16:43:02 -0400

    Fixed get_user_prefixes causing a segfault

--- src/ui/user_list.c
+++ src/ui/user_list.c
@@ -37,7 +37,7 @@ static inline int get_user_row(const struct buffer_info * buffer,
 
 static inline char * get_user_prefixes(const struct buffer_info * buffer,
                                        GtkTreeIter * user) {
-    GValue value;
+    GValue value = G_VALUE_INIT;
     char * prefixes;
     gtk_tree_model_get_value(GTK_TREE_MODEL(buffer->user_list_store),
                              user, 2, &value);
