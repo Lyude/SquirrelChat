5b4251e051b4989d2d24a546f0c15e748439f651
Lyude <thatslyude@gmail.com>
2013-12-19 13:09:53 -0500

    Added missing GPL notice in source file

--- src/ui/chat_viewer.c
+++ src/ui/chat_viewer.c
@@ -1,3 +1,20 @@
+/* Creates a new chat viewer
+ *
+ * Copyright (C) 2013 Stephen Chandler Paul
+ *
+ * This file is free software: you may copy it, redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of this License or (at your option) any
+ * later version.
+ *
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "chat_viewer.h"
 #include "chat_window.h"
 
@@ -10,3 +27,4 @@ void sqchat_chat_viewer_new(struct sqchat_chat_window * window) {
     gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(window->chat_viewer),
                                 GTK_WRAP_WORD_CHAR);
 }
+// vim: set expandtab tw=80 shiftwidth=4 softtabstop=4 cinoptions=(0,W4:
